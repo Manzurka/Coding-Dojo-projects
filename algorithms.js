ef31beea678fd52af3e09ebfc2fe5bd277f89195
Manzura <manzurkas@gmail.com>
2018-03-10 13:41:38 -0700

    "updated as of 3/9"

---
+++ algorithms.js
@@ -0,0 +1,10 @@
+function ReturnOddsArray1to255(){
+    var arr=[];
+    for (var val =1; val<=255; val+=2)
+    {
+        arr.push(val);
+    }
+    console.log(arr);
+}
+
+ReturnOddsArray1to255();
---
+++ functions.js
@@ -0,0 +1,8 @@
+functionPrint1to255(){
+    for(var i=1; i<=255; i++){
+        var total=0;
+        total=i+total;
+    }
+    
+}
+console.log(i,"the sum is");
